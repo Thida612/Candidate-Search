1fa5568a4b42f6a9b6a76aaf33cb6ad5978a7b28
Thida612 <angelic722@gmail.com>
2025-01-12 22:23:19 -0600

    Add state for current candidate and implement fetchCandidates function, Introduced  state to manage the currently selected candidate.Implemented  function to fetch candidate data from GitHub API and update the candidates list. Added console log to debug the search results.

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -12,9 +12,19 @@ interface Candidate {
   bio?: string;
 }
 
-const CandidateSearch = () => {
+  const CandidateSearch = () => {
   const [candidates, setCandidates] = useState<string[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [currentCandidate, setCandidate] = useState<Candidate | null>(null);
+
+  const fetchCandidates = async () => {
+    const data = await searchGithub();
+    console.log('searchGithub', data); // logs the search results
+    
+    // console.log('searchGithubUser', candidate); // logs the user's profile data
+    setCandidates(data.map((candidateOBJ:any) => candidateOBJ.login));
+
 
 
 
